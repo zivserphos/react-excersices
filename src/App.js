b8ae314fb219c158506cfe37925b2a2757e10289
zivserphos <zivfromisrael@gmail.com>
2021-11-17 20:12:11 +0200

    Organized my code directories and files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 /* eslint-disable no-unused-vars */
-import Greetings from "./Greetings";
 import Footer from "./components/footer/Footer";
 import Header from "./components/Header/Header";
-import Basket from "./components/main/Basket";
-import Search from "./components/main/main";
-import Groceries from "./components/main/groceries";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Main from "./components/main/main";
 
@@ -13,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return true;
+  }
+}
+
+export default Footer;
